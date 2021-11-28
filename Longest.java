f1b4725f6602f285d1b66d203f8314a619e7dd70
natalieycyoung <44858512+natalieycyoung@users.noreply.github.com>
2021-11-28 02:30:57 -0800

    Created classes, stubs

---
+++ Longest.java
@@ -0,0 +1,17 @@
+/**
+ * Programming Assignment 5
+ *
+ * @author Natalie Young
+ * @since 2021-11-02
+ */
+
+class Longest
+{
+	/**
+	 * Prints out longest string in command line args, nothing if no
+	 * args given, or alphabetically earliest string with longest
+	 * length if there is a tie
+	 */
+	public static void main(String[] args)
+	{}
+}
---
+++ Stats.java
@@ -0,0 +1,26 @@
+/**
+ * Programming Assignment 5
+ *
+ * @author Natalie Young
+ * @since 2021-11-02
+ */
+
+class Stats
+{
+	/**
+	 * If the first argument is:
+	 * "--product", print the product of the provided numbers
+	 * "--mean", print the average (mean) of the provided numbers
+	 * "--total", print the sum of the provided numbers
+	 * "--max", print the maximum of the provided numbers
+	 * "--min", print the minimum of the provided numbers
+	 * any other string, print "Bad option <arg>" where you will
+	 * replace "<arg>" with the first argument
+	 * 
+	 * Assume at least two command-line args (name of operation, at
+	 * least one number), and all args after first appropriate for
+	 * Double.parseDouble
+	 */
+	public static void main(String[] args)
+	{}
+}
